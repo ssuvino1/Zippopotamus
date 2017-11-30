c52da09093a47b3eb08fb652cc3b6967feaca5cf
Steve Suvino <ssuvino@gmail.com>
2017-11-30 02:31:39 -0500

    initial

---
+++ ZipTest/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Zippopotamus;
+
+
+namespace ZipTest
+{
+    class Program
+    {
+        static public void DisplayInput()
+        {
+            Console.WriteLine("====================================================");
+            Console.WriteLine("  Zippopotamus API Sample App");
+            Console.WriteLine("  1. for Zip Lookup");
+            Console.WriteLine("  2. for City Lookup");
+            Console.WriteLine("====================================================");
+            Console.Write("Zippopotamus\\>  ");
+        }
+        static string GetInput(string prompt)
+        {
+            string ret = string.Empty;
+            while (string.IsNullOrEmpty(ret))
+            {
+                Console.Write(prompt);
+                ret = Console.ReadLine();
+            }
+            return ret;
+        }
+        static private void DoZipLookup()
+        {
+            try
+            {
+                string country = GetInput("Country Abbreviation:  ");
+                string zipcode = GetInput("Zip Code:  ");
+                ZipData zipData = ZipCodeApi.GetDataByCountryAndPostalCode(country, zipcode);
+                if (zipData != null)
+                {
+                    Console.WriteLine("==========================================");
+                    Console.WriteLine("Search Parameters");
+                    Console.WriteLine("Country:     " + country);
+                    Console.WriteLine("Postal Code: " + zipcode);
+                    Console.WriteLine("==========================================");
+
+                    Console.WriteLine("  Country: " + zipData.Country);
+                    Console.WriteLine("  Abbr:    " + zipData.CountryAbbr);
+                    Console.WriteLine("  ZipCode: " + zipData.PostCode);
+                    foreach (ZipPlace zp in zipData.Places)
+                    {
+                        Console.WriteLine("\tName:       " + zp.Name);
+                        Console.WriteLine("\tState:      " + zp.State);
+                        Console.WriteLine("\tState Abbr: " + zp.Abbr);
+                        Console.WriteLine("\tLongtitude: " + zp.Long);
+                        Console.WriteLine("\tLatitude:   " + zp.Lat);
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static private void DoCityLookup()
+        {
+            try
+            {
+                string country = GetInput("Country Abbreviation:  ");
+                string state = GetInput("State Abbrevation:  ");
+                string city = GetInput("City:  ");
+                CityData CityData = ZipCodeApi.GetDataByCountryStateAndCity(country, state, city);
+                if (CityData != null)
+                {
+                    Console.WriteLine("==========================================");
+                    Console.WriteLine("Search Parameters");
+                    Console.WriteLine("Country:     " + country);
+                    Console.WriteLine("State Abbr:  " + state);
+                    Console.WriteLine("City:        " + city);
+                    Console.WriteLine("==========================================");
+
+                    Console.WriteLine("  Country:      " + CityData.Country);
+                    Console.WriteLine("  Country Abbr: " + CityData.CountryAbbr);
+                    Console.WriteLine("  State:        " + CityData.State);
+                    Console.WriteLine("  State Abbr:   " + CityData.StateAbbr);
+                    Console.WriteLine("  City:         " + CityData.City);
+                    foreach (CityPlace zp in CityData.Places)
+                    {
+                        Console.WriteLine("\tName:       " + zp.Name);
+                        Console.WriteLine("\tZip Code:   " + zp.PostCode);
+                        Console.WriteLine("\tLongtitude: " + zp.Long);
+                        Console.WriteLine("\tLatitude:   " + zp.Lat);
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void Main(string[] args)
+        {
+            string cmd = string.Empty;
+            DisplayInput();
+            while ((cmd = Console.ReadLine()).ToUpper() != "X")
+            {
+                switch (cmd)
+                {
+                    case "1":
+                        Console.WriteLine();
+                        DoZipLookup();
+                        break;
+                    case "2":
+                        Console.WriteLine();
+                        DoCityLookup();
+                        break;
+                }
+                DisplayInput();
+            }
+        }
+    }
+}
---
+++ Zippopotamus/Api.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Zippopotamus
+{
+    public static class ZipCodeApi
+    {
+        public static ZipData GetDataByCountryAndPostalCode(string country, string postalCode)
+        {
+            ZipData ret = null;
+            try
+            {
+                string url = string.Format("http://api.zippopotam.us/{0}/{1}", country, postalCode);
+                WebClient client = new WebClient();
+                string s = client.DownloadString(url);
+                ret = JsonConvert.DeserializeObject<ZipData>(s);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ret;
+        }
+        public static CityData GetDataByCountryStateAndCity(string country, string state, string city)
+        {
+            CityData ret = null;
+            try
+            {
+                string url = string.Format("http://api.zippopotam.us/{0}/{1}/{2}", country, state, city);
+                WebClient client = new WebClient();
+                string s = client.DownloadString(url);
+                ret = JsonConvert.DeserializeObject<CityData>(s);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ret;
+        }
+    }
+    public class CityPlace
+    {
+        [JsonProperty(PropertyName = "post code")]
+        public string PostCode { get; set; }
+        [JsonProperty(PropertyName = "place name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "longitude")]
+        public string Long { get; set; }
+        [JsonProperty(PropertyName = "latitude")]
+        public string Lat { get; set; }
+    }
+    public class ZipPlace : CityPlace
+    {
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+        [JsonProperty(PropertyName = "state abbreviation")]
+        public string Abbr { get; set; }
+    }
+    public class ZipData
+    {
+        [JsonProperty(PropertyName = "post code")]
+        public string PostCode { get; set; }
+        [JsonProperty(PropertyName = "country")]
+        public string Country { get; set; }
+        [JsonProperty(PropertyName = "country abbreviation")]
+        public string CountryAbbr { get; set; }
+        [JsonProperty(PropertyName = "places")]
+        public List<ZipPlace> Places { get; set; }
+    }
+    public class CityData
+    {
+        [JsonProperty(PropertyName = "place name")]
+        public string City { get; set; }
+        [JsonProperty(PropertyName = "country")]
+        public string Country { get; set; }
+        [JsonProperty(PropertyName = "country abbreviation")]
+        public string CountryAbbr { get; set; }
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+        [JsonProperty(PropertyName = "state abbreviation")]
+        public string StateAbbr { get; set; }
+        [JsonProperty(PropertyName = "places")]
+        public List<CityPlace> Places { get; set; }
+    }
+}
